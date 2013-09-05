3dc7aa77a8891b42c0cf37eb0d7862ee4a89a1d7
Kashif Iqbal Khan <kashiif@gmail.com>
2013-09-05 11:46:01 +0500

    Added po file and fixed l10n

--- src/plugin-template/inc/class-{plugin-slug}.php
+++ src/plugin-template/inc/class-{plugin-slug}.php
@@ -54,10 +54,6 @@ class {plugin-class-name} {
 		// Add the options page and menu item.
 		// add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
 
-		// Add an action link pointing to the options page.
-		// $plugin_basename = plugin_basename( plugin_dir_path( __FILE__ ) . '{plugin-slug}.php' );
-		// add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
-
 		// Load admin style sheet and JavaScript.
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
@@ -120,7 +116,7 @@ class {plugin-class-name} {
 		$locale = apply_filters( 'plugin_locale', get_locale(), $domain );
 
 		load_textdomain( $domain, WP_LANG_DIR . '/' . $domain . '/' . $domain . '-' . $locale . '.mo' );
-		load_plugin_textdomain( $domain, FALSE, basename( dirname( __FILE__ ) ) . '/lang/' );
+		load_plugin_textdomain( $domain, FALSE, {plugin-class-name-upper}_PLUGIN_NAME . '/lang/' );
 	}
 
 	/**
@@ -212,21 +208,6 @@ class {plugin-class-name} {
 		include_once( 'views/admin.php' );
 	}
 
-	/**
-	 * Add settings action link to the plugins page.
-	 *
-	 * @since    0.1.0
-	 */
-	public function add_action_links( $links ) {
-
-		return array_merge(
-			array(
-				'settings' => '<a href="' . admin_url( 'plugins.php?page={plugin-slug}' ) . '">' . __( 'Settings', $this->plugin_slug ) . '</a>'
-			),
-			$links
-		);
-
-	}
 
 	/**
 	 * NOTE:  Actions are points in the execution of a page or process
